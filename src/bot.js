b0e9cb03a7584694329d3e66a458d2a2cde7e4be
safonovklim <safonovklim@gmail.com>
2017-02-11 23:06:58 +0300

    Added mongo

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,9 @@
 import TelegramBot from 'node-telegram-bot-api'
 import BotConfig from '../config/bot.json'
 
+// Models
+import User from './db/models/User'
+
 let {token} = BotConfig
 
 export default class Bot {
@@ -10,7 +13,21 @@ export default class Bot {
 
   listener() {
     this.bot.on('message', (msg) => {
-      this.bot.sendMessage(msg.chat.id, 'хуй')
+      return Promise
+          .resolve()
+          .then(() => {
+            const user = msg.from;
+            const chat = msg.chat;
+            return User.create({
+                id: user.id,
+                username: user.username,
+                first_name: user.first_name,
+                last_name: user.last_name,
+            })
+          })
+          .then(() => {
+              this.bot.sendMessage(msg.chat.id, 'хуй')
+          });
     })
   }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Bot from './bot'
-let bot = new Bot()
-
+const bot = new Bot()
 
 bot.listener()
