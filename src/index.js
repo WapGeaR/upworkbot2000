93ec8c195139f5b8c84c90dcf239f0acb50f0aca
Anton Izmailov <wapgear@Antons-iMac.local>
2017-02-11 21:41:46 +0300

    latest channges with dummy example

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('ты пидор')
+import TelegramBot from 'node-telegram-bot-api'
+
+let token = '365374137:AAGlhMl6IQ92Foqka21m1mbvisp0hZYzWa4'
+
+class Bot {
+  constructor() {
+    this.bot = new TelegramBot(token, { polling: true })
+  }
+
+  method() {
+    this.bot.on('message', (msg) => {
+      this.bot.sendMessage(msg.chat.id, 'хуй')
+    })
+  }
+
+}
+
+let bot = new Bot(token, { polling: true })
+
+bot.method()
